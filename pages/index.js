03645f6ffa3102f9710abb3d9f0bbc667fd84692
Jishnudeep Bhattacharjee <bjishnudeep@gmail.com>
2021-11-14 17:38:37 +0530

    graphql data fetched

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,8 @@
 import Head from "next/head";
 import { PostCard, Categories, PostWidget } from "../components";
+import { getPosts } from "../services";
 
-const posts = [
-  { title: "React", excerpt: "Learn React Testing" },
-  { title: "Vue", excerpt: "learn Vue testing" },
-];
-
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -28,4 +24,12 @@ export default function Home() {
       </div>
     </div>
   );
+}
+
+// Fetch data at build time
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: { posts },
+  };
 }
