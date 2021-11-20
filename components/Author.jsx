73051893d5bc2450475e6472a0bb0574a6843ec1
Jishnudeep Bhattacharjee <bjishnudeep@gmail.com>
2021-11-20 17:45:54 +0530

    added author section

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Image from "next/Image";
 
-const Author = () => {
-  return <div>Author</div>;
+const Author = ({ author }) => {
+  return (
+    <div className="bg-black shadow-lg p-12 mt-20 mb-20 text-center relative rounded-lg bg-opacity-20">
+      <div className="absolute left-0 right-0 -top-14">
+        <Image
+          unoptimized
+          src={author.photo.url}
+          alt={author.name}
+          height="100px"
+          width="100px"
+          className="align-middle rounded-full"
+        />
+      </div>
+      <h3 className="text-white my-4 text-xl font-bold">{author.name}</h3>
+      <p className="text-white text-lg">{author.bio}</p>
+    </div>
+  );
 };
 
 export default Author;
