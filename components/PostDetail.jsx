161940d942d9b9b4c6e79f93511d31a25053804c
Jishnudeep Bhattacharjee <bjishnudeep@gmail.com>
2021-11-20 17:44:49 +0530

    style fixes

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -60,16 +60,18 @@ const PostDetail = ({ post }) => {
   };
 
   return (
-    <div className="bg-white shadow-lg rounded-lg lg:pb-12 p-8 mb-8">
-      <div className="relative overflow-hidden shadow-md mb-6">
-        <img
-          src={post.featuredImage.url}
-          alt={post.title}
-          className="object-top h-full w-full rounded-t-lg"
-        />
+    <>
+      <div className="bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8">
+        <div className="relative overflow-hidden shadow-md mb-6">
+          <img
+            src={post.featuredImage.url}
+            alt=""
+            className="object-top h-full w-full object-cover  shadow-lg rounded-t-lg lg:rounded-lg"
+          />
+        </div>
         <div className="px-4 lg:px-0">
           <div className="flex items-center mb-8 w-full">
-            <div className="flex items-center mb-4 lg:mb-0 w-full lg:w-auto mr-8 items-center">
+            <div className="hidden md:flex items-center justify-center lg:mb-0 lg:w-auto mr-8 items-center">
               <img
                 alt={post.author.name}
                 height="30px"
@@ -96,19 +98,22 @@ const PostDetail = ({ post }) => {
                   d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
                 />
               </svg>
-              <span>{moment(post.createdAt).format("MMM DD,YYYY")}</span>
+              <span className="align-middle">
+                {moment(post.createdAt).format("MMM DD, YYYY")}
+              </span>
             </div>
           </div>
+          <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
+          {post.content.raw.children.map((typeObj, index) => {
+            const children = typeObj.children.map((item, itemindex) =>
+              getContentFragment(itemindex, item.text, item)
+            );
+
+            return getContentFragment(index, children, typeObj, typeObj.type);
+          })}
         </div>
-        <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
-        {post.content.raw.children.map((typeObj, index) => {
-          const children = typeObj.children.map((item, itemIndex) =>
-            getContentFragment(itemIndex, item.text, item)
-          );
-          return getContentFragment(index, children, typeObj, typeObj.type);
-        })}
       </div>
-    </div>
+    </>
   );
 };
 
