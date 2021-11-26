6f4b2b905afe44bb0a99639bf67b0495789d99fe
Jishnudeep Bhattacharjee <bjishnudeep@gmail.com>
2021-11-26 17:33:21 +0530

    correct next/image path

--- components/FeaturedPostCard.jsx
+++ components/FeaturedPostCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import moment from "moment";
-import Image from "next/Image";
+import Image from "next/image";
 import Link from "next/link";
 
 const FeaturedPostCard = ({ post }) => (
